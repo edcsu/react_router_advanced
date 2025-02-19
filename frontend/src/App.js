59ac5cc00a0d5111462001c134ea978aac37d341
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-19 16:12:59 +0300

    refactor: place loader fx in events components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import RootLayout from "./RootLayout";
 import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
+import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
@@ -38,17 +38,7 @@ const router = createBrowserRouter([
       { path: 'events', 
         element: <EventsRoot />,
         children: [
-          { path: '', element: <EventsPage />, loader: async () => {
-            const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-                //
-              } else {
-                const resData = await response.json();
-                return resData.events
-              }
-            }
-          },
+          { path: '', element: <EventsPage />, loader: eventsLoader},
           { path: ':id', element: <EventDetailPage />},
           { path: 'new', element: <NewEventPage />},
           { path: ':id/edit', element: <EditEventPage />}
