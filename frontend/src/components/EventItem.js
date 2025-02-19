ef2284a6ad27a388cfb7c2266b39bd4063756538
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-19 15:13:25 +0300

    style: add link to the respective EventDetailPage

--- frontend/src/components/EventItem.js
+++ frontend/src/components/EventItem.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router';
 import classes from './EventItem.module.css';
 
 function EventItem({ event }) {
@@ -8,11 +9,13 @@ function EventItem({ event }) {
   return (
     <article className={classes.event}>
       <img src={event.image} alt={event.title} />
-      <h1>{event.title}</h1>
+      <NavLink to={event.id}>
+        <h1>{event.title}</h1>
+      </NavLink>
       <time>{event.date}</time>
       <p>{event.description}</p>
       <menu className={classes.actions}>
-        <a href="edit">Edit</a>
+        <NavLink href="edit">Edit</NavLink>
         <button onClick={startDeleteHandler}>Delete</button>
       </menu>
     </article>
