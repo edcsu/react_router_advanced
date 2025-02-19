4ea4a0db49882df32750079f868d4344f909363d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-19 16:39:36 +0300

    refactor: initial handling of errors

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -4,8 +4,15 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData()
+
+  if (data.isError) {
+    return (
+        <p>data.message</p>
+    )
+  }
+
   const events = data.events
-  
+
   return (
     <>
       <EventsList events={events} />
@@ -19,7 +26,10 @@ export const loader = async () => {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
-        //
+        return {
+            isError: true,
+            message: 'Could not fetch events'
+        }
     } else {
         return response
     }
