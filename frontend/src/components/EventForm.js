fcef5dce6403fcd0bc5b9eae479c336b12d3511f
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-20 11:27:47 +0300

    style: improve design on form submission

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,16 @@
 
-import { Form } from 'react-router';
+import { Form, useNavigate, useNavigation } from 'react-router';
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  
+  const navigate = useNavigate()
+  const navigation = useNavigation()
+
+  const isSubmitting = navigation.state === 'submitting'
+
   function cancelHandler() {
+    navigate('..')
   }
 
   return (
@@ -47,10 +54,12 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...'  : 'Save'}
+        </button>
       </div>
     </Form>
   );
